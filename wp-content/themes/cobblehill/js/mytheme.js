51e8bd2a0d405bd1569a43512c5e2abbbf5778a6
samanthavandapuye <samantha.vandapuye@gmail.com>
2014-06-03 13:15:53 -0400

    disable retinajs

--- wp-content/themes/cobblehill/js/mytheme.js
+++ wp-content/themes/cobblehill/js/mytheme.js
@@ -1,5 +1,6 @@
 $(window).bind('load', function() {
    $('#wrapper').delay(200).fadeIn('slow');
+	centerSliderNav();
 });
 
 $(window).load(function() {
@@ -21,12 +22,12 @@ $(window).load(function() {
 	    animation: "slide",
 		directionNav: false,
 		touch: true,
-		pauseOnAction: true, //Boolean: Pause the slideshow when interacting with control elements, highly recommended.
-		pauseOnHover: false, //Boolean: Pause the slideshow when hovering over slider, then resume when no longer hovering
-		useCSS: false //{NEW} Boolean: Slider will use CSS3 transitions if available
+		pauseOnAction: true, 
+		pauseOnHover: false, 
+		useCSS: false
 	  });
 	}
-	centerSliderNav()
+	centerSliderNav();
 	$('#contact-form .ajax-loader').attr('src', '/wp-content/themes/cobblehill/images/ajax-loader.gif');
 });
 
@@ -70,23 +71,18 @@ $(window).scroll(function() {
 			//$('#cat-cont').css({'position': 'relative', 'top': 0});
         }
 	}
+	if ($(window).width() > 1229) {
+		/* Check the location of each desired element */
+		$('.hideme').each( function(i){
+		    var bottom_of_object = $(this).position().top + $('#featured-cont').height();
+		    var bottom_of_window = $(window).scrollTop() + $(window).height() + 805;
+		    /* If the object is completely visible in the window, fade it it */
+		    if( bottom_of_window > bottom_of_object ){
+		        $(this).animate({'opacity':'1'},500);
+		    }
+		}); 
+	}
 	
-	    /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-
-            var bottom_of_object = $(this).position().top + $('#featured-cont').height();
-            var bottom_of_window = $(window).scrollTop() + $(window).height() + 805;
-
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-
-                $(this).animate({'opacity':'1'},500);
-
-            }
-
-        }); 
-	
-
 });
 
 
@@ -229,14 +225,7 @@ function loadMap() {
 		      title: 'Hello World!',
 			  icon: image
 		  });
-		var contentString =
-		'<div id="content"><div id="siteNotice"></div><img id="map-logo" src="/wp-content/themes/cobblehill/images/map-pin-img.jpg" style="position:relative; float:left; margin:25px;"/><div id="map-address">COBBLE HILL</br>329 East Bay St, 2nd Floor</br>Charleston, SC 29401</div></div>';
-		var infowindow = new google.maps.InfoWindow({
-		content: contentString
-		});
-		google.maps.event.addListener(marker, 'click', function() {
-		infowindow.open(map,marker);
-		});
+
 		var center;
 
 		function calculateCenter() {
@@ -248,6 +237,14 @@ function loadMap() {
 		google.maps.event.addDomListener(window, 'resize', function() {
 		  map.setCenter(center);
 		});
+		// var contentString =
+		// 	'<div id="content"><div id="siteNotice"></div><img id="map-logo" src="/wp-content/themes/cobblehill/images/map-pin-img.jpg" style="position:relative; float:left; margin:25px;"/><div id="map-address">COBBLE HILL</br>329 East Bay St, 2nd Floor</br>Charleston, SC 29401</div></div>';
+		// 	var infowindow = new google.maps.InfoWindow({
+		// 	content: contentString
+		// 	});
+		// 	google.maps.event.addListener(marker, 'click', function() {
+		// 	infowindow.open(map,marker);
+		// 	});
      }
     
 }
@@ -255,7 +252,7 @@ function loadMap() {
 function homeCtas() {
 	$('#cta-cont .cta').each(function() {
 		$(this).on({
-			mouseenter: function(){ $(this).children('.no-hover').hide(); $(this).children('.for-hover').css({'opacity':1}); $(this).children('.for-hover').fadeIn('slow'); },
+			mouseenter: function(){$(this).children('.no-hover').hide(); $(this).children('.for-hover').css({'opacity':1}); $(this).children('.for-hover').fadeIn('slow'); },
 			mouseleave: function(){$(this).children('.for-hover').hide(); $(this).children('.for-hover').css({'opacity':0}); $(this).children('.no-hover').fadeIn('slow');}
 		});
 	});
