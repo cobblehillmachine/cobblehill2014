d2388f3b889152aa7e1d92bbe5486fbe1915aad9
Trevor <tmwagner66@gmail.com>
2015-01-23 16:49:44 -0500

    more js fixes

--- wp-content/themes/cobblehill/js/mytheme.js
+++ wp-content/themes/cobblehill/js/mytheme.js
@@ -69,10 +69,10 @@ $(window).scroll(function() {
 			$('#header .hamburger').css({'background-position':'0 -28px'});
 			//$('#cat-cont').css({'position': 'fixed', 'top': 77});
         }else {
-            $('#header').removeClass('scrolled');$('#header .logo').addClass('white');
-			$(''#header .hamburger').css({'background-position':'0 0'});
+            $('#header').removeClass('scrolled');   $('#header .logo').addClass('white');
+			$('#header .hamburger').css({'background-position':'0 0'});
 			//$('#cat-cont').css({'position': 'relative', 'top': 0});
-        }
+      }
 	}
 	if ($(window).width() > 1229) {
 		/* Check the location of each desired element */
